a501734ecad2a123fc558558f490c7c712259edc
Pavel Durov <p3ld3v@pm.me>
2023-08-22 18:23:53 +0100

    prints and more prints

--- src/lyk.c
+++ src/lyk.c
@@ -6,40 +6,56 @@
 #include "ldebug.h"
 #include <stdlib.h>
 #include <yk.h>
- #include "stdio.h"
+#include "stdio.h"
 
 #define _DEFAULT_SOURCE /* for reallocarray() */
 
 static int LYK_VERBOSE = -1;
 
-int is_verbose() {
-  if (LYK_VERBOSE == -1){
+int is_verbose()
+{
+  if (LYK_VERBOSE == -1)
+  {
     LYK_VERBOSE = getenv("LYK_VERBOSE") != NULL;
   }
   return LYK_VERBOSE;
 }
 
-void log_debug(char *message) {
-  LYK_VERBOSE = is_verbose();
-  if (is_verbose()){
-    printf("[DEBUG] %s", message);
-  }
+int is_valid_yk_location(YkLocation loc) {
+  return loc.state != 0;
 }
 
-void print_proto_info(char *msg, Proto *f){
+void print_proto_info(char *msg, Proto *f)
+{
+  if (!is_verbose()){
+    return;
+  }
   char *source = getstr(f->source);
-   
   char *vars = NULL;
-
-  if (f->locvars != NULL && f->locvars->varname != NULL){
+  if (f->locvars != NULL && f->locvars->varname != NULL)
+  {
     vars = getstr(f->locvars->varname);
   }
   printf("[DEBUG] %s. \t f:\t%p \t source: %s: \t vars: %s \n", msg, f, source, vars);
+  if (f->yklocs != NULL){
+    for (int i = 0; i < f->sizecode; i++){
+      printf("[DEBUG]");
+      if (is_valid_yk_location(f->yklocs[i])){
+        printf("%p->yklocs[%d]=%p,", f, i, f->yklocs[i]);
+      }else{
+        printf("%p->yklocs[%d]=NULL,", f, i);
+      }
+      printf("\n");
+    }
+  }
 }
 
-
-void yk_new_proto(Proto *f) {
-  // print_proto_info("yk_new_proto", f);
+void yk_new_proto(Proto *f)
+{
+  if (is_verbose())
+  {
+    printf("yk_new_proto %p\n", f);
+  }
   f->yklocs = NULL;
 }
 /*
@@ -47,98 +63,92 @@ void yk_new_proto(Proto *f) {
  *    1.  They are loaded from text source via the `luaY_parser`.
  *    2.  They can also be loaded from binary representation using `luaU_undump`, where
  *        prototypes are previously dumped and saved.
- * 
- *  Yk tracing locations are allocated using both of these methods 
+ *
+ *  Yk tracing locations are allocated using both of these methods
  *  using `yk_set_location` and `yk_set_locations` respectively.
-*/
+ */
 
 /*
  *  Is the instruction `i` the start of a loop?
- *
- *  YKFIXME: Numeric and Generic loops can be identified by `OP_FORLOOP` and `OP_TFORLOOP` opcodes. 
- *  Other loops like while and repeat-until are harder to identify since they are based on 
- *  `OP_JMP` instruction.
+ *de
  */
-int is_loop_start(Instruction i) {
+int is_loop_start(Instruction i)
+{
   return (GET_OPCODE(i) == OP_FORLOOP || GET_OPCODE(i) == OP_TFORLOOP);
 }
 
-inline YkLocation *yk_lookup_ykloc(CallInfo *ci, Instruction *pc){
+inline YkLocation *yk_lookup_ykloc(CallInfo *ci, Instruction *pc)
+{
   YkLocation *ykloc = NULL;
   lua_assert(isLua(ci));
   Proto *p = ci_func(ci)->p;
   lua_assert(p->code <= pc && pc <= p->code + p->sizecode);
-  if (is_loop_start(*pc)) {
+  if (is_loop_start(*pc))
+  {
     ykloc = &p->yklocs[pc - p->code];
   }
   return ykloc;
 }
 
-inline void yk_set_location(Proto *f, Instruction i, int idx, int pc) {
-  if (f->yklocs == NULL){
-    print_proto_info("yk_set_location", f);
+inline void yk_set_location(Proto *f, Instruction i, int idx, int pc)
+{
+  if (f->yklocs == NULL)
+  {
+    print_proto_info("yk_set_location, calloc", f);
     f->yklocs = calloc(f->sizecode, sizeof(YkLocation));
-  } else {
-    // YKOPT: Reallocating for every instruction is inefficient.
-    f->yklocs = reallocarray(f->yklocs, pc, sizeof(YkLocation));
   }
-  
-  lua_assert(f->yklocs != NULL && "Expected yklocs to be defined!");
- 
+  else
+  {
+    print_proto_info("yk_set_location, reallocarray", f);
+    // YKOPT: Reallocating for every instruction is inefficient.
+    // f->yklocs = reallocarray(f->yklocs, pc, sizeof(YkLocation));
+  }
 
-  // for (int i = 0; i < f->sizecode; i++){
-  //   printf("yklocs[%d]=%p,", i, f->yklocs[i]); 
-  // }
-  // printf("\n");
+  lua_assert(f->yklocs != NULL && "Expected yklocs to be defined!");
   if (is_loop_start(i))
   {
     print_proto_info("yk_location_new", f);
     f->yklocs[idx] = yk_location_new();
   }
-
-
 }
 
-inline void yk_init_proto(Proto *f) {
+inline void yk_init_proto(Proto *f)
+{
   f->yklocs = calloc(f->sizecode, sizeof(YkLocation));
   lua_assert(f->yklocs != NULL && "Expected yklocs to be defined!");
   print_proto_info("yk_init_proto", f);
-  for (int i = 0; i < f->sizecode; i++){
-     if (is_loop_start(i)){
+  for (int i = 0; i < f->sizecode; i++)
+  {
+    if (is_loop_start(i))
+    {
       print_proto_info("yk_location_new", f);
       f->yklocs[i] = yk_location_new();
-    }else{
-      // f->yklocs[i] = NULL;
     }
   }
-
-  for (int i = 0; i < f->sizecode; i++){
-    printf("yklocs[%d]=%p,", i, f->yklocs[i]); 
-  }
-  printf("\n");
 }
 
-
-inline void yk_free_proto(Proto *f) {
+inline void yk_free_proto(Proto *f)
+{
   print_proto_info("yk_free_proto", f);
-  // YK locations are initialised as close as possible to the function loading, 
+  // YK locations are initialised as close as possible to the function loading,
   // However, this load can fail before we initialise `yklocs`.
   // This NULL check is a workaround for that.
-  if (f->yklocs != NULL) {
-    for (int i = 0; i < f->sizecode; i++) {
-      if (is_loop_start(i) && &f->yklocs[i] != NULL) {
+  if (f->yklocs != NULL)
+  {
+    for (int i = 0; i < f->sizecode; i++)
+    {
+      if (is_loop_start(i) && &f->yklocs[i] != NULL)
+      {
         print_proto_info("yk_location_drop", f);
         YkLocation loc = f->yklocs[i];
-        printf("[DEBUG] Detected loop location. f->yklocs[%d]=%p\n", i, loc);
-        
-        // if (&loc == NULL){
-        //   printf("@@@@@@@@@@@@@@ YEY\n");
-        // }else{
-        //   printf("@@@@@@@@@@@@@@ NEY\n");
-        // }
-        // printf("@@@@@@@@@@@@@@ NADA\n");
-        // TODO: continue here
-        yk_location_drop(f->yklocs[i]);
+        // YkLocations are set with calloc which that 0 bytes are written to the memory.
+        // But YkLocations themselves can be uninitialized.
+        // This check makes sure that we only free locations that are initialised via `yk_location_new` function.
+        printf("[DEBUG] Detected loop location. %p->yklocs[%d]=%p\n", f, i, loc);
+        if (is_valid_yk_location(f->yklocs[i])){
+          printf("[DEBUG] Dropped. %p->yklocs[%d]=%p\n", f, i, loc);
+          yk_location_drop(loc);
+        }
       }
     }
     free(f->yklocs);
