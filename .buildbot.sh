99f68d802a92aee6372fddb67efa5d6ed8e288da
Pavel Durov <p3ld3v@pm.me>
2023-08-24 17:29:32 +0100

    nothing works

--- .buildbot.sh
+++ .buildbot.sh
@@ -45,9 +45,5 @@ cd tests
 # run within reasonable time).
 LUA=../src/lua
 for serialise in 0 1; do
-    for test in api bwcoercion closure code events \
-        gengc pm tpack tracegc vararg goto cstack locals; do
-        echo "### YKD_SERIALISE_COMPILATION=$serialise $test.lua ###"
-        YKD_SERIALISE_COMPILATION=$serialise ${LUA} -e"_U=true" ${test}.lua
-    done
+    YKD_SERIALISE_COMPILATION=$serialise ${LUA} -e"_U=true" all.lua
 done
