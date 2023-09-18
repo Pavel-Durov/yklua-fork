2af0de1ddea7e40abc483757a250dd8ca8ea51e7
Pavel Durov <p3ld3v@pm.me>
2023-09-18 16:49:32 +0100

    Partial success

---
+++ tests/yk/test_runner.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <assert.h>
+// #include "lobject.h"
+
+// Define your test functions here
+void test_addition() {
+    // Your test code here
+    // assert(1 == 2 && "expected for math to not work!");
+    // printf("test_addition!\n");
+}
+
+void expect_uk_locations_to_not_be_set() {
+    // Your test code here
+    // assert(1 == 2 && "expected for math to not work!");
+    // Proto *p = pe
+}
+
+int main() {
+    // Run your test functions
+    test_addition();
+    expect_uk_locations_to_not_be_set();
+
+    printf("All tests passed!\n");
+    return 0;
+}
